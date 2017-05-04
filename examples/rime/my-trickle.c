2aa75e9e07b8ecbd8c75b182c207b4040ea8593f
Ben Hadley <bhadley2@gmail.com>
2017-05-04 12:27:58 -0700

    Change message to node id

--- examples/rime/my-trickle.c
+++ examples/rime/my-trickle.c
@@ -12,6 +12,9 @@
 #include "random.h"
 
 #include <stdio.h>
+
+static uint8_t node;
+
 /*---------------------------------------------------------------------------*/
 PROCESS(example_trickle_process, "Trickle example");
 AUTOSTART_PROCESSES(&example_trickle_process);
@@ -19,9 +22,9 @@ AUTOSTART_PROCESSES(&example_trickle_process);
 static void
 trickle_recv(struct trickle_conn *c)
 {
-  printf("%d.%d: trickle message received '%s'\n",
+  printf("%d.%d: trickle message received '0x%02x'\n",
 	 linkaddr_node_addr.u8[0], linkaddr_node_addr.u8[1],
-	 (char *)packetbuf_dataptr());
+	 ((uint8_t *)packetbuf_dataptr())[0]);
 }
 const static struct trickle_callbacks trickle_call = {trickle_recv};
 static struct trickle_conn trickle;
@@ -35,13 +38,16 @@ PROCESS_THREAD(example_trickle_process, ev, data)
 
   trickle_open(&trickle, CLOCK_SECOND, 145, &trickle_call);
 
+  node = 0;
+  node |= (1 << (linkaddr_node_addr.u8[0] - 1));
+
   while(1) {
     /* Delay 2-4 seconds */
     etimer_set(&et, CLOCK_SECOND * 2 + random_rand() % (CLOCK_SECOND * 2));
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-    packetbuf_copyfrom("Hello, world", 13);
+    packetbuf_copyfrom(&node, 1);
     trickle_send(&trickle);
     printf("trickle message sent\n");
   }
